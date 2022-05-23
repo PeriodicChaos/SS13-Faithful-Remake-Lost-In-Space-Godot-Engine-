462f9f914fe4bea16e0e1fdc5a6f0dd2ac8d8f45
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2022-05-23 05:55:51 +0200

    Adds basic smoothed out movement

--- SS13 Faithful Remake - (Lost in Space)/Scripts/Player.gd
+++ SS13 Faithful Remake - (Lost in Space)/Scripts/Player.gd
@@ -1,14 +1,18 @@
 extends KinematicBody2D
 
 onready var animationPlayer = $AnimationPlayer
-onready var ray = $RayCast2D
-var grid_size = 32
+onready var ray : RayCast2D = $RayCast2D
+var grid_size   : int = 32
 var inputs = {
 	'ui_up': Vector2.UP,
 	'ui_down': Vector2.DOWN,
 	'ui_left': Vector2.LEFT,
 	'ui_right': Vector2.RIGHT
 }
+
+var movement_queue : Vector2
+var movement_speed : float = 0.65
+var is_moving      : bool = false
 
 func _process(_delta):
 	var left = Input.is_action_just_pressed("ui_right")
@@ -27,17 +31,26 @@ func _process(_delta):
 
 func _unhandled_input(event):
 	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(dir)
+		if event.is_action(dir):
+			movement_input(dir)
+			
+func _physics_process(delta):
+	if(movement_queue != position and is_moving): move()
 
-func move(dir):
+func move():
+	position = lerp(position, movement_queue, movement_speed)
+	if(movement_queue.is_equal_approx(position)): is_moving = false
+				
+func movement_input(dir):
 	var vector_pos = inputs[dir] * grid_size
 	ray.cast_to = vector_pos
 	ray.force_raycast_update()
-	if !ray.is_colliding():
-		position += vector_pos
+	if ray.is_colliding() == false and is_moving == false:
+		is_moving = true
+		movement_queue = vector_pos + position
 	else:
 		var collider = ray.get_collider()
+		if(collider == null): return
 		if collider.is_in_group('Closet'):
 			if collider.move(dir):
 				position += vector_pos
